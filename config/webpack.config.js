5cec273786813874c8be030712ad31ba0f3de875
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-21 20:53:58 +0100

    fixed issue with

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -472,8 +472,9 @@ module.exports = function (webpackEnv) {
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
-                modules: true,
-                localIdentName: '[name]__[local]__[hash:base64:5]',
+                modules: {
+                  localIdentName: '[name]__[local]___[hash:base64:5]'
+                },
                 sourceMap: isEnvProduction
                   ? shouldUseSourceMap
                   : isEnvDevelopment,
