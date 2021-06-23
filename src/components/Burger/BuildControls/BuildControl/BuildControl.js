11a4e3a2ce1e47bc3ee926be30d50c1baa518be4
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-23 17:31:58 +0100

    we can now dynamically added things to our burger

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,7 @@ const buildControl = (props) => (
     <div className={classes.buildControl}>
         <div className={classes.label}>{props.label}</div>
         <button className={classes.buttonLess}>Less</button>
-        <button className={classes.buttonMore}>More</button>
+        <button className={classes.buttonMore} onClick={props.added}>More</button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -18,10 +25,14 @@ class BurgerBuilder extends Component {
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
-        const updatedIngredient = {
+        const updatedIngredients = {
             ...this.state.ingredients
         };
-        updatedIngredient[type] = updatedCount;
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     };
 
     removeIngredientHandler = (type) => {};
@@ -30,6 +41,8 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={ this.state.ingredients } />
+                <BuildControls
+                    ingredientAdded={ this.addIngredientHandler } />
             </Aux>
         );
     }
