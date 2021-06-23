b9ddb6834354c6fe42d003fba0a6f18bb4ddf88a
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-23 17:13:07 +0100

    updating styling

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,8 +3,8 @@ import React from 'react';
 import classes from './BuildControl.css';
 
 const buildControl = (props) => (
-    <div>
-        <div>{props.label}</div>
+    <div className={classes.buildControl}>
+        <div className={classes.label}>{props.label}</div>
         <button className={classes.buttonLess}>Less</button>
         <button className={classes.buttonMore}>More</button>
     </div>
