3267c83459f16cab89636b95d6690bc6a5f0506a
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-23 17:23:49 +0100

    adding a addIngredientHandler and a removeIngredientHandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
@@ -10,8 +11,20 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredient = {
+            ...this.state.ingredients
+        };
+        updatedIngredient[type] = updatedCount;
+    };
+
+    removeIngredientHandler = (type) => {};
 
     render () {
         return (
