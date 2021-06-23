a9c0336ccdd0a1ec26962be99f2da67f225884d6
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-23 15:51:23 +0100

    setting up BuildControls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+
+const buildControls = (props) => (
+    <div className={classes.buildControls}>
+
+    </div>
+);
+
+export default buildControls;
