c7126c735b0c6f5e34679018258d2c801119935d
Becky Wilson <rebecca.wilson2@sky.uk>
2021-07-13 20:41:16 +0100

    changed the Layout component to a class

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.css';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDraw from '../Navigation/SideDraw/SideDraw';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDraw />
-        <main className={classes.content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDraw />
+                <main className={classes.content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+};
 
-export default layout;
+export default Layout;
