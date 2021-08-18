d35a14f91182eb4f4c66b02ac6856e042a761703
Becky Wilson <rebecca.wilson2@sky.uk>
2021-08-18 21:03:00 +0100

    added burger menu to nav bar

--- src/components/Navigation/SideDraw/DrawToggle/DrawToggle.js
+++ src/components/Navigation/SideDraw/DrawToggle/DrawToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './DrawToggle.css';
 
 const drawToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.drawToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawToggle;
